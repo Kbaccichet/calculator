268e59dd9d5908c258277a1b84a4ebcd7d0d2c4f
Kristof Baccichet <kristof.baccichet@gmail.com>
2023-09-02 13:44:28 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+
+def main():
+    x=add(1,2)
+    y=sub(5,3)
+    print(x)
+    print(y)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main() 
